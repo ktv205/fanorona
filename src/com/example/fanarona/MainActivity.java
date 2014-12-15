b6edc8dd6e6947ca6593c8cb9514121d4f89a1f2
krishna teja velagapudi <ktv205@nyu.edu>
2014-12-14 22:04:56 -0500

    basic game is complete but did not use alpha beta pruning the first move legal is considered

--- src/com/example/fanarona/MainActivity.java
+++ src/com/example/fanarona/MainActivity.java
@@ -5,6 +5,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 import android.app.Activity;
+import android.app.ProgressDialog;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
@@ -25,6 +26,7 @@ public class MainActivity extends Activity implements OnClickListener {
 	int MoveFlag = 0;
 	int firstMove;
 	int secondMove;
+	int winFlag;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -81,6 +83,7 @@ public class MainActivity extends Activity implements OnClickListener {
 	@Override
 	public void onClick(View v) {
 		// int id = v.getId();
+		checkForWin();
 		String resourceName = getResources().getResourceName(v.getId());
 		Log.d(TAG, "onClick->resourceName->" + resourceName);
 		int resourceNumber = Integer.valueOf(resourceName
@@ -105,9 +108,10 @@ public class MainActivity extends Activity implements OnClickListener {
 				}
 			}
 
-		} else {
-			Log.d(TAG, "figure out fucking computer move please");
 		}
+	}
+	public void checkForWin(){
+		
 	}
 
 	public void figureOutConfig() {
@@ -116,7 +120,7 @@ public class MainActivity extends Activity implements OnClickListener {
 		int secondAdjFlag = 0;
 		int difference;
 		Log.d(TAG, "firstMove->" + firstMove);
-		Log.d(TAG, "firstMove->" + secondMove);
+		Log.d(TAG, "secondMove->" + secondMove);
 		int firstId = getResources().getIdentifier("image" + firstMove, "id",
 				this.getPackageName());
 		int secondId = getResources().getIdentifier("image" + secondMove, "id",
@@ -133,7 +137,8 @@ public class MainActivity extends Activity implements OnClickListener {
 		}
 		if (firstAdjFlag == 1) {
 			difference = firstMove - secondMove;
-			for (int i = 0; i < adjTwo.length; i++) {
+			int i = 0;
+			while (i < adjTwo.length) {
 				if (boardConfig[adjTwo[i]] == 1) {
 					Log.d(TAG, "thirdMove->" + adjTwo[i]);
 					int thirdId = getResources().getIdentifier(
@@ -148,23 +153,44 @@ public class MainActivity extends Activity implements OnClickListener {
 						firstImage.setImageResource(R.drawable.blue);
 						secondImage.setImageResource(R.drawable.black);
 						thirdImage.setImageResource(R.drawable.blue);
+						boardConfig[firstMove] = 2;
+						boardConfig[secondMove] = 3;
+						boardConfig[adjTwo[i]] = 2;
+						for (int j = 0; j < boardConfig.length; j++) {
+							Log.d(TAG, "after move->boardConfig->" + j + "->"
+									+ boardConfig[j]);
+						}
 						secondAdjFlag = 1;
 						playerFlag = 1;
+						Log.d(TAG, "figure out fucking computer move please");
+						new FanaronaAsycTask().execute();
+						i = adjTwo.length;
 					} else if (adjTwo[i] == firstMove + (2 * difference)) {
 						Log.d(TAG,
 								"inFigureOutConfig->atttack->figured out third move");
 						firstImage.setImageResource(R.drawable.blue);
 						secondImage.setImageResource(R.drawable.black);
 						thirdImage.setImageResource(R.drawable.blue);
+						boardConfig[firstMove] = 2;
+						boardConfig[secondMove] = 3;
+						boardConfig[adjTwo[i]] = 2;
+						for (int j = 0; j < boardConfig.length; j++) {
+							Log.d(TAG, "after move->boardConfig->" + j + "->"
+									+ boardConfig[j]);
+						}
 						secondAdjFlag = 1;
 						playerFlag = 1;
-
+						Log.d(TAG, "figure out fucking computer move please");
+						new FanaronaAsycTask().execute();
+						i = adjTwo.length;
 					}
 				}
+				i++;
 			}
 			if (secondAdjFlag == 0) {
+				i = 0;
 				difference = firstMove - secondMove;
-				for (int i = 0; i < adjOne.length; i++) {
+				while (i < adjOne.length) {
 					if (secondMove != adjOne[i]) {
 						if (boardConfig[adjOne[i]] == 1) {
 							if (firstMove + difference == adjOne[i]) {
@@ -177,23 +203,48 @@ public class MainActivity extends Activity implements OnClickListener {
 								firstImage.setImageResource(R.drawable.blue);
 								secondImage.setImageResource(R.drawable.black);
 								thirdImage.setImageResource(R.drawable.blue);
+								boardConfig[firstMove] = 2;
+								boardConfig[secondMove] = 3;
+								boardConfig[adjOne[i]] = 2;
+								for (int j = 0; j < boardConfig.length; j++) {
+									Log.d(TAG, "after move->boardConfig->" + j
+											+ "->" + boardConfig[j]);
+								}
+
 								secondAdjFlag = 1;
 								playerFlag = 1;
+								Log.d(TAG,
+										"figure out fucking computer move please");
+								new FanaronaAsycTask().execute();
+								i = adjOne.length;
 							}
 						}
 					}
+					i++;
 				}
+			}
+		}
+		if (firstAdjFlag == 1) {
+			if (secondAdjFlag == 0) {
+				firstImage.setImageResource(R.drawable.blue);
+				secondImage.setImageResource(R.drawable.black);
+				boardConfig[firstMove] = 2;
+				boardConfig[secondMove] = 3;
+				playerFlag = 1;
+				secondAdjFlag = 1;
+				new FanaronaAsycTask().execute();
 			}
 		}
 	}
 
 	public class FanaronaAsycTask extends
 			AsyncTask<Void, Void, ArrayList<Integer>> {
+		ProgressDialog dialog;
 
 		@Override
 		protected void onPreExecute() {
-			// TODO Auto-generated method stub
-			super.onPreExecute();
+			dialog = new ProgressDialog(MainActivity.this);
+			dialog.show();
 		}
 
 		@Override
@@ -201,6 +252,10 @@ public class MainActivity extends Activity implements OnClickListener {
 			ArrayList<Integer> black = new ArrayList<Integer>();
 			ArrayList<Integer> blue = new ArrayList<Integer>();
 			ArrayList<Integer> red = new ArrayList<Integer>();
+			for (int i = 0; i < boardConfig.length; i++) {
+				Log.d(TAG, "doInBackBround->boardConfig->" + i + "->"
+						+ boardConfig[i]);
+			}
 			for (int i = 0; i < boardConfig.length; i++) {
 				if (boardConfig[i] == 1) {
 					red.add(i);
@@ -218,74 +273,122 @@ public class MainActivity extends Activity implements OnClickListener {
 			int secondMoveFlag = 0;
 			int thirdMoveFlag = 0;
 			while (redPosition != red.size()) {
+				Log.d(TAG, "firstMove in backGround->" + red.get(redPosition));
 				firstMove = red.get(redPosition);
 				firstMoveFlag = 1;
-				int[] firstAdj = adjacent.get(red.get(redPosition));
+				int[] firstAdj = adjacent.get(firstMove);
 				int i = 0;
 				while (i < firstAdj.length) {
 					if (boardConfig[firstAdj[i]] == 2) {
+						Log.d(TAG, "secondMove in backGround->" + firstAdj[i]);
 						secondMove = firstAdj[i];
 						secondMoveFlag = 1;
 						int[] secondAdj = adjacent.get(secondMove);
 						int difference = firstMove - secondMove;
-						for (int j = 0; j < secondAdj.length; j++) {
+						Log.d(TAG, "difference between first and second Move->"
+								+ difference);
+						int j = 0;
+						while (j < secondAdj.length) {
 							if (boardConfig[secondAdj[j]] == 3) {
-								Log.d(TAG, "thirdMove->" + secondAdj[j]);
-								thirdMove = secondAdj[j];
-								Log.d(TAG,
-										"inFigureOutConfig->attack->figuring out third move");
+								Log.d(TAG, "figuring out third move->"
+										+ secondAdj[j]);
 								if (secondAdj[j] == firstMove
 										- (2 * difference)) {
+									Log.d(TAG,
+											"figured out third move-> attack->"
+													+ secondAdj[j]);
 									thirdMove = secondAdj[j];
 									thirdMoveFlag = 1;
-									Log.d(TAG,
-											"inFigureOutConfig->figured out third move");
-
+									j = secondAdj.length;
 								} else if (secondAdj[j] == firstMove
 										+ (2 * difference)) {
+									Log.d(TAG,
+											"figured out third move-> attack->"
+													+ secondAdj[j]);
 									thirdMoveFlag = 1;
 									thirdMove = secondAdj[j];
-									Log.d(TAG,
-											"inFigureOutConfig->atttack->figured out third move");
-
+									j = secondAdj.length;
 								}
 							}
+							j++;
 						}
 						if (thirdMoveFlag == 0) {
-							difference = firstMove - secondMove;
-							for (int j = 0; j < firstAdj.length; j++) {
+							j = 0;
+							while (j < secondAdj.length) {
 								if (secondMove != firstAdj[j]) {
-									if (boardConfig[firstAdj[j]] == 1) {
+									if (boardConfig[firstAdj[j]] == 3) {
 										if (firstMove + difference == firstAdj[j]) {
-											thirdMove = firstAdj[i];
+											Log.d(TAG,
+													"figured out third move-> retrive->"
+															+ secondAdj[j]);
+											thirdMove = firstAdj[j];
 											thirdMoveFlag = 1;
+											j = secondAdj.length;
 										}
 									}
 								}
+								j++;
 							}
 						}
-
 					}
-					if (secondMoveFlag == 0) {
+					if (secondMove == 0) {
 						i++;
-					} else if (thirdMoveFlag == 0) {
+					} else if (thirdMove == 0) {
 						i++;
-					} else if (thirdMoveFlag == 1) {
+					} else if (thirdMove != 0) {
 						i = firstAdj.length;
 					}
 				}
-				if (thirdMoveFlag == 1) {
-					redPosition = red.size();
-				} else {
+				if (thirdMove == 0) {
 					redPosition++;
+				} else {
+					redPosition = red.size();
 				}
 			}
+
 			ArrayList<Integer> finalList = new ArrayList<Integer>();
 			finalList.add(firstMove);
 			finalList.add(secondMove);
 			finalList.add(thirdMove);
 			return finalList;
+
 		}
 
+		@Override
+		protected void onPostExecute(ArrayList<Integer> result) {
+			dialog.dismiss();
+			Log.d(TAG,
+					"firstMove->" + result.get(0) + "secondMove->"
+							+ result.get(1) + "thirdMove->" + result.get(2));
+			int firstId = getResources().getIdentifier("image" + result.get(0),
+					"id", MainActivity.this.getPackageName());
+			int secondId = getResources().getIdentifier(
+					"image" + result.get(1), "id",
+					MainActivity.this.getPackageName());
+			int thirdId = getResources().getIdentifier("image" + result.get(2),
+					"id", MainActivity.this.getPackageName());
+			ImageView firstImage = (ImageView) findViewById(firstId);
+			ImageView secondImage = (ImageView) findViewById(secondId);
+
+			ImageView thirdImage = (ImageView) findViewById(thirdId);
+			firstImage.setImageResource(R.drawable.blue);
+			secondImage.setImageResource(R.drawable.red);
+			if (thirdImage == null) {
+
+			} else {
+				thirdImage.setImageResource(R.drawable.blue);
+				boardConfig[result.get(2)] = 2;
+			}
+			boardConfig[result.get(0)] = 2;
+			boardConfig[result.get(1)] = 1;
+
+			for (int i = 0; i < boardConfig.length; i++) {
+				Log.d(TAG, "doInBackBround->boardConfig->" + i + "->"
+						+ boardConfig[i]);
+			}
+			playerFlag = 0;
+			MoveFlag = 0;
+		}
 	}
+
 }
